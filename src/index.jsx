3a15544f9f354550aa00932d757b09bb4f959543
oksang <dev.oksang@gmail.com>
2021-03-16 21:48:01 +0900

    웹팩 설치 및 기본 파일 세팅

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+    return (
+        <p>Hello, React</p>
+    );
+} 
+
+ReactDOM.render(<App></App>, document.getElementById('app'));
